4b45f5e4e621a6b0fe0abe96b428c8a57a5dab50
jerry <aafulin@gmail.com>
2022-10-24 20:59:58 +0800

    boostrap表單版型

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TypedFormDemoComponent } from './typed-form-demo/typed-form-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypedFormDemoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/typed-form-demo/typed-form-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typed-form-demo',
+  templateUrl: './typed-form-demo.component.html',
+  styleUrls: ['./typed-form-demo.component.css']
+})
+export class TypedFormDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
