0f3dc0a7791cf0e99df4dd581f981c9fa51e91c8
jerry <aafulin@gmail.com>
2022-10-25 21:02:10 +0800

    建立強型別表單的第一種方式

--- src/app/typed-form-demo/typed-form-demo.component.ts
+++ src/app/typed-form-demo/typed-form-demo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-typed-form-demo',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./typed-form-demo.component.css']
 })
 export class TypedFormDemoComponent implements OnInit {
+  cities = ['Taipei', 'Taichung', 'Kaohsiung'];
+  // infer by TypeScript
+  form = this.fb.group({
+    email: this.fb.control('i@me.com'),
+    password: this.fb.control('abc123'),
+    address: this.fb.control('No. 201, Furong Rd., Houli Dist.'),
+    city: this.fb.control('Taichung'),
+    zip: this.fb.control('421'),
+    checkMeOut: this.fb.control(true),
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
